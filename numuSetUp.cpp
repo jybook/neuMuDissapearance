2595a37245cbc4ed3fb6112049e9769c461f5644
jybook <julia.y.book@gmail.com>
2018-06-19 11:00:46 -0500

    Update numuSetUp.cpp
    Successfully takes the values from the cross section file and puts them into an array with the same binning at the flux file.

--- numuSetUp.cpp
+++ numuSetUp.cpp
@@ -2,15 +2,12 @@
 #include <math.h>
 //Get the data we need in the form we need it
 void numuSetUp(){
-	
+
 	TGraph* crossSection = 0;//holds cross-section graph
 	TFile *crossSectionFile = TFile::Open("cross_section.root");
-	//crossSectionFile ->GetObject("qel_cc_n", crossSection);
+	crossSectionFile ->GetObject("qel_cc_n", crossSection);
 
 	//get an array holding the x values of the graph from 0 to 10 GeV
-	//double csXAxis [] = crossSection->GetX();
-	//double csXAxis [] = qel_cc_n->GetX();
-	int xPoints = qel_cc_n -> GetN();//sizeof(csXAxis);
 	int relevantLength = qel_cc_n -> GetN();
 	cout << "number of points in the graph =" << relevantLength << endl;
 	
@@ -19,7 +16,7 @@ void numuSetUp(){
 	TFile *fluxFile = TFile::Open("flux.root");
 	fluxFile ->GetObject("numu_CV_AV_TPC", flux);
 
-	flux->Draw();
+	//flux->Draw();
 
 	int numBins = numu_CV_AV_TPC ->GetNbinsX();
 	double binFlux; //number of neutrinos produced at the energy corresponding to a particular bin
@@ -33,36 +30,24 @@ void numuSetUp(){
 	double crossSecHist[200];//crossSecHist[numBins];
 	double binMean;
 	//double binSum;
-	int x;
-	int y;
-	int x2;
-	int y2;
+	double x;
+	double y;
+	double x2;
+	double y2;
 	int currentPoint=0;
 
+	crossSection ->Draw();
+	
 
-	crossSection -> Draw();
-	for(int i=0; i<numBins; i++, currentPoint += ppb){
+	for(int i=0; i<numBins; i++,currentPoint += ppb){
+		//cout << "Current point: "<< currentPoint << " ";
 		crossSection->GetPoint(currentPoint, x, y);
 		crossSection->GetPoint(currentPoint + ppb - 1, x2, y2);
+		//cout << "x values: " << x << " , " << x2 << " ";
+		//cout << "y values: " << y << " , " << y2 << " ";
 		binMean = (y+y2)/2;
 		crossSecHist[i] = binMean;
-	}
-/*
-
-	for(int i=0; i<numBins; i++){
-		binSum = 0;
-		for(int j=0; j<ppb; j++){
-			crossSection->GetPoint(currentPoint, x, y);
-			binSum = binSum + y;
-
-			//last thing, move to the next point
-			currentPoint++;
-		}
-		binMean = binSum/ppb;
-		crossSecHist[i] = binMean;
-	}*/
-
-	cout <<crossSecHist << endl;
+		//cout << "the mean for bin "<< i << " is " << binMean << endl;
 
 return;
 }
