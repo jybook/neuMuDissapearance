15aa97575dc35f6c127f450b55da23182e3bf8c1
jybook <julia.y.book@gmail.com>
2018-06-19 17:26:11 -0500

    Update numuSetUp_2.cpp
    Working verison

--- numuSetUp_2.cpp
+++ numuSetUp_2.cpp
@@ -3,27 +3,24 @@
 
 void codeTester(){
 	
-	double flux[];
+	double flux[200];
 	double crossSection[200];
 
 	crossSection[0] = 24;
 	cout <<"crossSection address: "<< crossSection << endl;
 
-	numuSetUp(crossSection);
+	numuSetUp(crossSection, flux);
 	cout << "numuSetUp completed" << endl;
 	cout <<"crossSection address final: "<< crossSection << endl;
-
-	cout << crossSection[0] << endl;
-	cout << crossSection[0] << " , " << crossSection[100] << " , " << crossSection[185] << endl;
-	
+	cout << "the flux is: ";
+	printArr(flux);
+	cout << "the cross-section is: ";
+	printArr(crossSection);
 
 }
 
-void numuSetUp(double *crossSecArr){
-	cout << "numuSetUp entered" << endl;
+void numuSetUp(double *crossSecArr, double *fluxArr){
 	
-	cout <<"crossSection address2: "<< crossSecArr << endl;
-
 	TGraph* crossSection = 0;//holds cross-section graph
 	TFile *crossSectionFile = TFile::Open("cross_section.root");
 	crossSectionFile ->GetObject("qel_cc_n", crossSection);
@@ -41,7 +38,6 @@ void numuSetUp(double *crossSecArr){
 	int ppb = relevantLength/numBins;//Get points per pin. relevantLength/numBins may not be an integer -- will this cause problems?
 
 	//compute the mean value of the cross section for every "bin" of evergy, place it in an array
-	//double crossSecHist[200];//crossSecHist[numBins];
 	double binMean;
 	double x, y, x2, y2;
 	int currentPoint=0;
@@ -50,22 +46,25 @@ void numuSetUp(double *crossSecArr){
 		crossSection->GetPoint(currentPoint, x, y);
 		crossSection->GetPoint(currentPoint + ppb - 1, x2, y2);
 		binMean = (y+y2)/2;
-		//crossSecHist[i] = binMean;
 		crossSecArr[i] = binMean;
 	}
-	cout << crossSecArr[0] << " , " << crossSecArr[100] << " , " << crossSecArr[185] << endl;
-	cout << "numuSetUp almost done" << endl;
-	
-	//cout << *crossSecHist << endl;
-	cout << *crossSecArr << endl;
-	
-	//*crossSecArr = *crossSecHist;
-	//cout << "crossSecHist address: "<< crossSecHist << endl;
-	cout << *crossSecArr << endl;
-	
-	cout << crossSecArr[100] <<endl;
-	//cout << "if true, it will say "<< true<< " "<< (crossSecArr[185] == crossSecHist[185])<< endl;
-	cout << "current array address: "<< crossSecArr << endl;
+
+	TH1 *flux = 0; //holds flux histogram
+	TFile *fluxFile = TFile::Open("flux.root");
+	fluxFile ->GetObject("numu_CV_AV_TPC", flux);
+
+	for(int i=0; i<200; i++){
+		fluxArr[i] = flux -> GetBinContent(i);
+	}
 
 return;
+
+}
+
+void printArr(double *a1){
+	int length = 200;
+	for(int i=0; i<length; i++){
+		cout << a1[i] << ", ";
+	}
+	cout << endl;
 }
