59f7ef84571657db759a1437d861c2349cec8efc
jybook <julia.y.book@gmail.com>
2018-06-21 15:12:44 -0500

    Update numuDissapearance.cpp
    Fixed the normalization factor

--- numuDissapearance.cpp
+++ numuDissapearance.cpp
@@ -2,8 +2,14 @@
 #include <math.h>
 
 //in this script, it's assumed that the flux file has 200 bins with widths of 50 MeV each
+int nBins = 200;
 
-void numuDissapearance(){
+void codeTester(){
+	cout << "Start setup" << endl;
+	int xbins = 10;
+	int ybins = 300;
+	double xmax = .10;
+	double ymax = 10.0;
 	
 	TCanvas *c1 = new TCanvas("c1", "Event Rates", 1200, 600);
 	c1 -> Divide(2, 1, 0.01, 0.01, 0);// divide(number of pads in the x direction, number of pads in the y direction, x-margin, y-margin, color)
@@ -13,42 +19,64 @@ void numuDissapearance(){
 	double crossSection[200];
 	numuSetUp(crossSection, flux);
 
-	//calculate event rate in the ICARUS TPC
+	//calculate event rate at 470m
 	double eventRate[200];
 	calculateEventRate(eventRate, flux, crossSection);
-	TH1 *eventRateHist = new TH1D("eventRateHist", "Predicted Event Rate at 470m; Neutrino Energy (GeV); Event Rate", 200, 0.0, 10.1);
 	
-	for(int j=0; j<200; j++){
-		eventRateHist -> SetBinContent(j, eventRate[j]);
-	}
-	//c1 -> cd(1);
-	//eventRateHist ->Draw();
-
+	//adjust event rate for the ICARUS TPC
 	TH1 *eventRateICARUS = new TH1D("eventRateICARUS", "Predicted Event Rate at 600m (no oscillations); Neutrino Energy (GeV); Event Rate", 200, 0.0, 10.1);
 	double scale = (470.0*470)/(600.0*600);
 	double binRate;
 	for(int j=0; j<200; j++){
 		binRate = scale*eventRate[j];
 		eventRateICARUS -> SetBinContent(j, binRate);
+	} 
+	cout << "end setup" << endl;
+	//Display event rate in ICARUS for the null hypothesis
+	//c1 -> cd(1);//cd(2);
+	//eventRateICARUS -> Draw();
+
+	//calculate event rate in ICARUS for the fourth oscillation hypothesis with a particular delta m^2, sin^2(2theta)
+	TH1 *oscRateICARUS = new TH1D("oscRateICARUS", "Predicted Event Rate at 600m (with oscillations); Neutrino Energy (GeV); Event Rate", 200, 0.0, 10.01);
+	cout << "line 40 done" << endl;
+	
+	TH2 *dissapearanceProb = new TH2D("dissapearanceProb", "Dissapearance Chi-squareds in ICARUS; Sin^2(2 Theta); Delta m^2", xbins, 0.0, xmax, ybins, 0.0, ymax);
+	cout << "line 43 done" << endl;
+	
+	double xres = xmax/xbins;
+	double yres = ymax/ybins;
+	cout << "line 47 done" << endl;
+	cout << "start ";
+	cout << "start2 ";
+
+	for(int i=0; i<xbins; i++){
+		cout << "in loop, i = " << i << endl;
+		for (int j=0; j<ybins; j++){
+			double ss2t = xres*i;
+			double dMSquared = yres*j;
+			calculateOscillation(dMSquared, ss2t, 0.600, eventRateICARUS, oscRateICARUS);
+			double chiSquared = calculateChiSquared(eventRateICARUS, oscRateICARUS);
+			//Draw a contour at about 1.65
+			if((chiSquared >= 1.6) && (chiSquared <= 1.7)){chiSquared = 999999;}
+			dissapearanceProb -> SetBinContent(i, j, chiSquared);
+			}
 	}
-	c1 -> cd(1);//cd(2);
-	eventRateICARUS -> Draw();
-
-	TH1 *oscRateICARUS = new TH1D("oscRateICARUS", "Predicted Event Rate at 600m (with oscillations); Neutrino Energy (GeV); Event Rate", 200, 0.0, 10.1);
-	calculateOscillation(1.0, .010, 0.600, eventRateICARUS, oscRateICARUS);
+	cout << "end" << endl;
+	
+	//Display event rate in ICARUS for the fourth oscillation hypothesis
 	c1 -> cd(2);//cd(3);
-	oscRateICARUS -> Draw();
+	c1_2 -> SetLogx(); c1_2 -> SetLogy(); c1_2 -> SetLogz();
+	dissapearanceProb -> Draw("COLZ");
 
-	double chiSquared = calculateChiSquared(eventRateICARUS, oscRateICARUS);
 }
 
 void calculateOscillation(double dMS, double ss2Th, double oscLength, TH1 *eventRate, TH1 *result){//calculate oscillation probability for a given (delta M squared, sin^2(2Theta)) pair, at L, given a particular event rate
 	//General formula for P(E) numu -> numu: 1 - sin^2(2*theta) * sin^2(1.27(delta m^s)(L/E))
-	int nBins = eventRate -> GetNbinsX();
+	//int nBins = eventRate -> GetNbinsX();
 	double binWidth = 0.05; // bin width in Gev = 50 MeV
 	double binProb =0;
 	double arg = 0;
-	double binEnergy = 0;
+	double binEnergy = 0.025;
 	double er = 0; //event rate for the current bin
 
 	for(int i=0; i<nBins; i++){
@@ -62,26 +90,31 @@ void calculateOscillation(double dMS, double ss2Th, double oscLength, TH1 *event
 }
 
 double calculateChiSquared(TH1 *noOsc, TH1 *osc){
-	double chiSquared = 0;
-	int nBins = 200;//noOsc -> GetNBinsX();
+	double chiSquared = 0.0;
+	//int nBins = 200;//noOsc -> GetNBinsX();
 
 	for(int i=0; i<nBins; i++){
 		double nNull = noOsc -> GetBinContent(i);
-		if (nNull == 0){}
+		if (nNull == 0){
+			chiSquared += 0;
+		}
 		else{
 		double nOsc = osc -> GetBinContent(i);
-		chiSquared += (pow((nNull - nOsc), 2)/nNull);
+		double increment = (pow((nNull - nOsc), 2)/nNull);
+		chiSquared += increment;
 		}
 	
 	}
-	cout << "chiSquared is " << chiSquared << endl;
+
+	//cout << " chiSquared: " << chiSquared << endl;
 	return chiSquared;
 
 }
 
 void calculateEventRate(double *eventRateArr, double *crossSecArr, double *fluxArr){
 	for(int i=0; i<200; i++){
-		eventRateArr[i] = crossSecArr[i]*fluxArr[i]*(7.176*(10^30));
+		//eventRateArr[i] = crossSecArr[i]*fluxArr[i]*(7.176*(10^30));
+		eventRateArr[i] = crossSecArr[i]*fluxArr[i]*(7.176*(100)*6);
 	}
 }
 
