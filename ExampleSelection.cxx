7a335ef84c44d7d98bb41326b7442a89c7eeb4f8
jybook <julia.y.book@gmail.com>
2018-09-26 11:01:57 -0500

    Update ExampleSelection.cxx

--- ExampleSelection.cxx
+++ ExampleSelection.cxx
@@ -6,6 +6,7 @@
 #include <THStack.h>
 #include <TLegend.h>
 #include <TCanvas.h>
+#include <TLine.h>
 #include <TH1D.h>
 #include <json/json.h>
 #include "gallery/ValidHandle.h"
@@ -28,11 +29,22 @@ void ExampleSelection::Initialize(Json::Value* config) {
   // Make a histogram
   fNuVertexXZHist = new TH2D("nu_vtx_XZ", "",
                              100, -1000, 1000, 100, -1000, 1000);
+
+  fModeAndVertex = new TH2D("mode_at_vtx_XZ", "",
+                             100, -1000, 1000, 100, -1000, 1000);
+
+  fDaughterAndVertex = new TH2D("daughters_at_vtx_XZ", "",
+                             100, -1000, 1000, 100, -1000, 1000);
+
+  top = new TLine(-199.15, 500, 199.15, 500);
+  left = new TLine(-199.15, 0, -199.15, 500);
+  bottom = new TLine(-199.15, 0, 199.15, 0);
+  right = new TLine(199.15, 0, 199.15, 500);
+
   phID1 = 0;
   phID2 = 0;
   phID3 = 0;
   phID4 = 0;
-
   numBins = 100;
   minBin = 0.0;
   maxBin = 2.5;
@@ -51,6 +63,7 @@ void ExampleSelection::Initialize(Json::Value* config) {
   fFluxTag = { "generator" };
   fParticleTag = { "generator" };
 
+
   if (config) {
     fMyParam = { (*config)["ExampleAnalysis"].get("parameter", 0).asInt() };
     fTruthTag = { (*config)["ExampleAnalysis"].get("MCTruthTag", "generator").asString() };
@@ -62,7 +75,9 @@ void ExampleSelection::Initialize(Json::Value* config) {
   AddBranch("nucount", &fNuCount);
   AddBranch("myvar", &fMyVar);
   AddBranch("parentHad", &fParentHad);
-
+  AddBranch("interactionType", &fInteractionType);
+  AddBranch("interactionMode", &fMode);
+  AddBranch("daughter", &fDaughter);
   // Use some library code
   hello();
 }
@@ -71,16 +86,18 @@ void ExampleSelection::Finalize() {
   // Output our histograms to the ROOT file
   fOutputFile->cd();
   fNuVertexXZHist->Write();
+  fModeAndVertex -> Write();
+  fDaughterAndVertex -> Write();
 
   fPHadron1 -> SetFillColor(kCyan-7);
   fPHadron2 -> SetFillColor(kViolet-4);
   fPHadron3 -> SetFillColor(kGreen-3);
-  fPHadron4 -> SetFillColor(kBlue);
+  
   fPHadron1 -> Write();
   fPHadron2 -> Write();
   fPHadron3 -> Write();
   fPHadron4 -> Write();
-
+   
   const char *ph1char = (std::to_string(phID1)).c_str();
   const char *ph2char = (std::to_string(phID2)).c_str();
   const char *ph3char = (std::to_string(phID3)).c_str();
@@ -90,14 +107,23 @@ void ExampleSelection::Finalize() {
   fLegend -> AddEntry(fPHadron2, ph2char, "f");
   fLegend -> AddEntry(fPHadron3, ph3char, "f");
   fLegend -> AddEntry(fPHadron4, ph4char, "f");
-
+   
   fPHStack->Add(fPHadron1);
   fPHStack->Add(fPHadron2);
   fPHStack->Add(fPHadron3);
   fPHStack->Add(fPHadron4);
 
+  fCanvas = new TCanvas("fCanvas", "", 700, 500);
+  fCanvas -> cd();
+  fModeAndVertex -> Draw("COLZ");
+  right ->Draw("al same");
+  left -> Draw("l same");
+  top  -> Draw("l same");
+  bottom -> Draw("l same");
+  fCanvas -> Write();
+
   fStackCanvas = new TCanvas("fStackCanvas", "", 700, 500);
-  fStackCanvas -> cd();
+  fStackCanvas -> cd(); 
   fPHStack -> Draw();
   fLegend -> Draw();
   fStackCanvas -> Write();
@@ -113,7 +139,6 @@ bool ExampleSelection::ProcessEvent(const gallery::Event& ev, std::vector<Event:
   auto const& mctruths = *ev.getValidHandle<std::vector<simb::MCTruth>>(fTruthTag);
   auto const& mcfluxes = *ev.getValidHandle<std::vector<simb::MCFlux>>(fFluxTag);
   auto const& mcparticles = *ev.getValidHandle<std::vector<simb::MCParticle>>(fParticleTag);
-
   // Fill in the custom branches
   fNuCount = mctruths.size();  // Number of neutrinos in this event
   fMyVar = fMyParam;
@@ -135,11 +160,29 @@ bool ExampleSelection::ProcessEvent(const gallery::Event& ev, std::vector<Event:
    else if(fParentHad == phID3 || phID3 == 0) { fPHadron3 ->  Fill(fNuEnergy); phID3 = fParentHad; }
    else if(fParentHad == phID4 || phID4 == 0) { fPHadron4 ->  Fill(fNuEnergy); phID4 = fParentHad; }
 
+   fNumDaughters = mctruth.GetNeutrino().Nu().NumberDaughters();
+   fDaughter = mctruth.GetNeutrino().Nu().FirstDaughter();
+/*
+   for(int i = 1; i<fNumDaughters; i++){
+      fDaughter = std::next (fDaughter);   
+        }    
+*/
+   fMode = mctruth.GetNeutrino().Mode();
+   int interactionNumber = (fInteractionType-1000)*100;
+
    //Fill neutrino vertex position histogram
    fNuVertexXZHist->Fill(mctruth.GetNeutrino().Nu().Vx(),
                         mctruth.GetNeutrino().Nu().Vz());
-  }
 
+   fModeAndVertex -> Fill(mctruth.GetNeutrino().Nu().Vx(),
+                        mctruth.GetNeutrino().Nu().Vz(), mctruth.GetNeutrino().Mode()+1);
+/*   
+   if(mctruth.GetNeutrino().CCNC() == 1 ){
+   fDaughterAndVertex -> Fill(mctruth.GetNeutrino().Nu().Vx(),
+                        mctruth.GetNeutrino().Nu().Vz(), fDaughter);
+    }*/
+                                                                                                                                                                            156,3         93%
+ }
   return true;
 }
 
@@ -149,7 +192,6 @@ bool ExampleSelection::ProcessEvent(const gallery::Event& ev, std::vector<Event:
 
 // This line must be included for all selections!
 DECLARE_SBN_PROCESSOR(ana::ExampleAnalysis::ExampleSelection)
+                                                                                                                                                                            196,5         Bot
 
-                                                                                                                                   153,0-1       Bot
-
-                                       
+                                                                                                                                                                            95,5          31%
